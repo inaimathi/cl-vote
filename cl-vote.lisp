1d74535ad53461c6f0c10e081943aa82a9c834d1
inaimathi <leo.zovic@gmail.com>
2017-07-27 18:50:12 -0400

    Working on the voting page

--- cl-vote.lisp
+++ cl-vote.lisp
@@ -8,22 +8,42 @@
 			  for href = (gethash :link p)
 			  if href do (htm (:li (:a :href href (str (gethash :title p)))))
 			  else do (htm (:li (str (gethash :title p)))))))))
-      (htm
-       (:html
-	(:head (:title "Vote"))
-	(:body
-	 (:div
-	  (:h1 "Reading Schedule")
-	  (papers-list (get-scheduled-papers)))
-	 (:div
-	  (:h3 "Future Papers")
-	  (papers-list (get-future-papers)))
-	 (:div
-	  (:h3 "Past Papers")
-	  (papers-list (get-past-papers)))))))))
+      (htm (:html (:head (:title "home - Papers"))
+		  (:body
+		   (:div
+		    (:h1 "Reading Schedule")
+		    (papers-list (get-scheduled-papers)))
+		   (:div
+		    (:h3 "Future Papers")
+		    (papers-list (get-future-papers)))
+		   (:div
+		    (:h3 "Past Papers")
+		    (papers-list (get-past-papers)))))))))
 
-(define-handler (api/vote :method :post :content-type "application/json") ()
-  (list :todo "Submit a vote slate"))
+(define-handler (ballot) ()
+  (with-html-output-to-string (s nil :prologue t :indent t)
+    (:html
+     (:head (:title "vote - Papers"))
+     (:body
+      (:form :action "/api/vote/redirecting" :method "POST"
+       (:ul
+	(loop for p in (get-future-papers)
+	   for href = (gethash :link p)
+	   do (htm (:li (:input :type "checkbox" :name (gethash :id p) :value "1")
+			(if href
+			    (htm (:a :href href :target "_blank"
+				     (str (gethash :title p))))
+			    (str (gethash :title p)))))))
+       (:input :type "submit"))))))
+
+(define-handler (api/vote/redirecting :method :post) ()
+  (loop for (k . v) in (parameters request)
+     collect (cons (parse-integer (symbol-name k)) 1))
+  (redirect! "/"))
+
+(define-json-handler (api/vote :method :post) ()
+  (loop for (k . v) in (parameters request)
+     collect (cons (parse-integer (symbol-name k)) 1)))
 
 (define-handler (api/paper :method :put :content-type "application/json") ()
   (list :todo "Submit a new paper for voting"))
