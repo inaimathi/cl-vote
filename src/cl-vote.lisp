8772764120b51356dc39527ccb8b2dfa680dc3f0
inaimathi <leo.zovic@gmail.com>
2020-09-10 20:59:29 -0400

    Start working on votes

--- src/cl-vote.lisp
+++ src/cl-vote.lisp
@@ -13,8 +13,9 @@
   (substitute
    #\- #\space
    (string-trim
-    " \n" (string-downcase
-	   candidate-title))))
+    (list #\space #\newline)
+    (string-downcase
+     candidate-title))))
 
 (defmacro page-template (&body contents)
   `(who:with-html-output-to-string (*standard-output*)
@@ -92,7 +93,7 @@
       (:li (:input :type "submit" :value "Login"))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;; elections
+;;;;; elections and voting
 (house:define-handler (action/election/create) (title ballot-type min max points candidates)
   (assert (house:lookup :user house:session))
   (assert (contains? #{"points" "range"} ballot-type))
@@ -121,17 +122,43 @@
       (:ul
        (:li (:input :type "text" :name "title" :placeholder "Election Title"))
        (:li (:input :type "radio" :checked "true" :name "ballot-type" :value "points")
-	    (:label :for "range" "Points")
+	    (:label :for "ballot-type" "Points")
 	    (:input :type "text" :name "points" :placeholder "Number of Points" :value "1"))
        (:li (:input :type "radio" :name "ballot-type" :value "range")
-	    (:label :for "range" "Range")
+	    (:label :for "ballot-type" "Range")
 	    (:input :type "text" :name "min" :placeholder "Min" :value "0")
 	    (:input :type "text" :name "max" :placeholder "Max" :value "5"))
+       (:li (:input :type "radio" :name "ballot-type" :value "order")
+	    (:label :for "ballot-type" "Order"))
        (:li (:textarea :name "candidates" :placeholder "Either comma separated or one-per-line"))
+       ;; (:li (:input :type "checkbox" :name "voter-nomination")
+       ;; 	    (:label :for "voter-nomination" "Voters Can Nominate Candidates"))
        (:li (:input :type "submit" :value "Create")))))))
 
-(house:define-handler (action/election/vote) ((election-id string) (ballot json))
-  (format nil "~a" (house:parameters house:request)))
+(defun ballot-params->vote (user election params)
+  (let ((ballot-type (lookup election :ballot-type))
+	(u-id (lookup user :id)))
+    (case (first ballot-type)
+      (:order {:todo t :voter u-id})
+      (:points
+       (let ((pts (second ballot-type)))
+	 (cond
+	   ((= 1 pts)
+	    (let ((vote (cdr (assoc :vote params))))
+	      (assert (stringp vote))
+	      {:voter u-id :params (parse-integer vote)
+	       :todo t :type "one-point-ballot"}))
+	   ((>= 5 pts)
+	    {:todo t :type "2-5-point-ballot"})
+	   (t {:todo t :type ">5-point-ballot"}))))
+      (:range {:todo t}))))
+
+(house:define-handler (action/election/vote) ((election-id string))
+  (format nil "ELECTION ~a: ~a" election-id
+	  (ballot-params->vote
+	   (house:lookup :user house:session)
+	   (election-by-id (parse-integer election-id))
+	   (house:parameters house:request))))
 
 (defun candidate-input (ballot-type candidate)
   (let* ((b-type (first ballot-type))
@@ -139,37 +166,42 @@
 	 (max (third ballot-type))
 	 (points (second ballot-type))
 	 (name (lookup candidate :name))
+	 (id (lookup candidate :id))
 	 (safe (sanitized-name name)))
     (cl-who:with-html-output (*standard-output*)
-      (cond ((and (== b-type :points) (= 1 points))
+      (cond ((and (== b-type :order))
 	     (cl-who:htm
-	      (:li (:input :type "radio" :name "vote" :value safe)
+	      (:li (:input :type "text" :name "vote")
+		   (:label :for "vote" (cl-who:str (format nil "~a - TODO" name))))))
+	    ((and (== b-type :points) (= 1 points))
+	     (cl-who:htm
+	      (:li (:input :type "radio" :name "vote" :value id)
 		   (:label :for "vote" (cl-who:str name)))))
 	    ((and (== b-type :range) (= min 0) (= max 1))
 	     (cl-who:htm
-	      (:li (:input :type "checkbox" :name safe)
-		   (:label :for safe (cl-who:str name)))))
+	      (:li (:input :type "checkbox" :name id)
+		   (:label :for id (cl-who:str name)))))
 	    ((and (== b-type :points) (>= 5 points))
 	     (cl-who:htm
 	      (:li (loop for i from 0 repeat (+ points 1)
-		      do (cl-who:htm (:input :type "radio" :name safe :value i)))
-		   (:label :for safe (cl-who:str name)))))
+		      do (cl-who:htm (:input :type "radio" :name id :value i)))
+		   (:label :for id (cl-who:str name)))))
 	    ((== b-type :points)
 	     (cl-who:htm
-	      (:li (:input :type "text" :name safe :value "0")
-		   (:label :for safe (cl-who:str name)))))
+	      (:li (:input :type "text" :name id :value "0")
+		   (:label :for id (cl-who:str name)))))
 	    ((and (== b-type :range) (= min -1) (= max 1))
 	     (cl-who:htm
 	      (:li "TODO - up and downvote arrows")))
 	    ((and (== b-type :range) (> 10 (- max min)))
 	     (cl-who:htm
 	      (:li (loop for i from min to max
-		      do (cl-who:htm (:input :type "radio" :name safe :value i)))
-		   (:label :for safe (cl-who:str name)))))
+		      do (cl-who:htm (:input :type "radio" :name id :value i)))
+		   (:label :for id (cl-who:str name)))))
 	    ((== b-type :range)
 	     (cl-who:htm
-	      (:li (:input :type "range" :name safe :min min :max max :value "0")
-		   (:label :for safe (cl-who:str name)))))))))
+	      (:li (:input :type "range" :name id :min min :max max :value "0")
+		   (:label :for id (cl-who:str name)))))))))
 
 (house:define-handler (election) ((election-id string))
   (assert (house:lookup :user house:session))
@@ -179,11 +211,15 @@
       (:h3 (cl-who:str (lookup election :title)))
       (:form
        :action "/action/election/vote"
+       (:input :type "hidden" :name "election-id" :value election-id)
        (:ul :class "ballot"
 	    (mapcar
 	     (lambda (cand)
 	       (candidate-input ballot-type cand))
-	     (lookup election :candidates)))))))
+	     (sort (copy-list (lookup election :candidates))
+		   (lambda (a b)
+		     (string<= (lookup a :name) (lookup b :name))))))
+       (:input :type "submit" :value "Vote")))))
 
 (house:define-handler (election/manage) (election-id) :todo)
 
